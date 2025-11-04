51de20c94ecb9dc185e650dba5dc356a3d0ece25
Kajitaka <142304219+kajitaka1201@users.noreply.github.com>
2025-11-04 08:16:19 +0000

    fix bugs

--- types/database.ts
+++ types/database.ts
@@ -1,19 +1,41 @@
-import type { Todo } from "./todo";
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
 export interface Database {
   public: {
     Tables: {
       todos: {
-        Row: Todo;
+        Row: {
+          id: string;
+          title: string;
+          completed: boolean;
+          created_at: string;
+        };
         Insert: {
+          id?: string;
           title: string;
           completed?: boolean;
+          created_at?: string;
         };
         Update: {
+          id?: string;
           title?: string;
           completed?: boolean;
+          created_at?: string;
         };
+        Relationships: [];
       };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
     };
   };
 }
